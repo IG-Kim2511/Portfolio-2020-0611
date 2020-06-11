192b42da703fc8327187343a58876b012914e26a
IanKim <59094101+IanKim@users.noreply.github.com>
2020-06-11 15:10:44 -0400

    css: ~~about jobs

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
 "use strict";
+
+//js refresh page every 2 seconds (완성 이후 지우기)
+
+setTimeout(function () {
+  window.location.reload(1);
+}, 8000);
+
+// c58. make navbar transparent when it is on the top
+
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
